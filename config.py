737386e847d55d82c24ea7be0e2c3b575c4e2d86
Sergey Botalov <89090168690@mail.ru>
2025-11-28 14:10:54 +0500

    addde db

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config:
     """Base config app"""
 
     SECRET_KEY = os.environ.get("SECRET_KEY") or "super-secret-"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(
+        basedir, "app.db"
+    )
