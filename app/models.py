cd8c6c696d1e92bfcd1f66000ded614006cc1f39
Botalov Sergey <89090168690@mail.ru>
2025-11-29 20:25:20 +0500

    added register

--- app/models.py
+++ app/models.py
@@ -1,11 +1,18 @@
 from datetime import datetime, timezone
 from typing import Optional
+from werkzeug.security import generate_password_hash, check_password_hash
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from flask_login import UserMixin
+from app import db, login
 
 
-class User(db.Model):
+@login.user_loader
+def load_user(id: str | int) -> "User":
+    return db.session.get(User, int(id))
+
+
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(
         sa.String(64),
@@ -20,6 +27,12 @@ class User(db.Model):
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(260))
 
     posts: so.WriteOnlyMapped["Post"] = so.relationship(back_populates="author")
+
+    def set_password(self, password: str) -> None:
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password: str) -> bool:
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f"<User {self.username}>"
