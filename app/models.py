8193d6d9c1c0d9bc19f3e9d0d0d9ca0c7a84dd07
Botalov Sergey <89090168690@mail.ru>
2025-11-30 20:18:10 +0500

    added edit profile

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timezone
 from typing import Optional
+from hashlib import md5
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlalchemy as sa
 import sqlalchemy.orm as so
@@ -25,6 +26,10 @@ class User(UserMixin, db.Model):
         unique=True,
     )
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(260))
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(200))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc)
+    )
 
     posts: so.WriteOnlyMapped["Post"] = so.relationship(back_populates="author")
 
@@ -33,6 +38,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password: str) -> bool:
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode("utf-8")).hexdigest()
+        return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"
 
     def __repr__(self):
         return f"<User {self.username}>"
